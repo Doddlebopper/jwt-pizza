abc9ae3829eefc48af987c471301d96f56f32675
Nicholas Samson <slicknd07@gmail.com>
2025-11-02 16:43:58 -0700

    fixed secrets for workflow completion

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import istanbul from 'vite-plugin-istanbul';
 export default defineConfig({
   build: { sourcemap: true },
   define: {
-    'import.meta.env.VITE_PIZZA_SERVICE_URL': JSON.stringify(process.env.VITE_PIZZA_SERVICE_URL || 'http://localhost:3000'),
-    'import.meta.env.VITE_PIZZA_FACTORY_URL': JSON.stringify(process.env.VITE_PIZZA_FACTORY_URL || 'http://localhost:3001'),
+    'import.meta.env.VITE_PIZZA_SERVICE_URL': JSON.stringify(process.env.VITE_PIZZA_SERVICE_URL || 'https://pizza-service.doddlebopper.click'),
+    'import.meta.env.VITE_PIZZA_FACTORY_URL': JSON.stringify(process.env.VITE_PIZZA_FACTORY_URL || 'https://pizza-service.doddlebopper.click'),
   },
   plugins: [
     istanbul({
